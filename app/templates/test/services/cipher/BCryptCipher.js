af3a14beaedcf600e0064ca78a7602562f061b71
Eugene Obrezkov <ghaiklor@gmail.com>
2015-02-12 14:32:20 +0200

    Add test for bcrypt and base ciphers

--- app/templates/test/services/cipher/BCryptCipher.js
+++ app/templates/test/services/cipher/BCryptCipher.js
@@ -1,7 +1,20 @@
-var assert = require('assert');
+var assert = require('assert'),
+    BCryptCipher = require('../../../api/services/cipher/BCryptCipher');
 
 describe("cipher:BCryptCipher", function () {
-    it("TODO: write this test", function () {
-        assert(true);
+    it("Should be a function", function () {
+        assert.equal(typeof BCryptCipher, 'function');
+    });
+
+    it("Should properly create new instance", function () {
+        var cipher = new BCryptCipher('test');
+        assert(cipher instanceof BCryptCipher);
+    });
+
+    it("Should properly hash data", function () {
+        var cipher = new BCryptCipher('test'),
+            hash = cipher.hashSync();
+
+        assert(new BCryptCipher(hash).compareSync('test'));
     });
 });
--- app/templates/test/services/cipher/BaseCipher.js
+++ app/templates/test/services/cipher/BaseCipher.js
@@ -1,7 +1,22 @@
-var assert = require('assert');
+var assert = require('assert'),
+    BaseCipher = require('../../../api/services/cipher/BaseCipher');
 
 describe("cipher:BaseCipher", function () {
-    it("TODO: write this test", function () {
-        assert(true);
+    it("Should be a function", function () {
+        assert.equal(typeof BaseCipher, 'function');
+    });
+
+    it("Should properly create BaseCipher", function () {
+        var cipher = new BaseCipher('test');
+        assert(cipher instanceof BaseCipher);
+    });
+
+    it("Should properly get/set content", function () {
+        var cipher = new BaseCipher('test');
+
+        assert.equal(cipher.getContent(), 'test');
+
+        cipher.setContent({id: '123'});
+        assert.deepEqual(cipher.getContent(), {id: '123'});
     });
 });
